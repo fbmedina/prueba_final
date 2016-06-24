5e2351eedf514942b110c9a6851ce99ed147eccc
Francisca Medina <fbmedina@uc.cl>
2016-06-23 21:49:36 -0400

    added column category to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160624013430) do
+ActiveRecord::Schema.define(version: 20160624014851) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20160624013430) do
     t.integer  "role"
     t.integer  "ranking"
     t.text     "cv"
+    t.string   "category"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
