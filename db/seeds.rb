103a718d2df6573f52240a5d54b2701f8c3b28fa
Francisca Medina <fbmedina@uc.cl>
2016-06-23 21:43:32 -0400

    Generated seeds of users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.destroy_all
+
+25.times do |u|
+  User.create!(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: "password",
+    cv: Faker::Lorem.sentence(7),
+    ranking: Faker::Number.between(1, 5),
+    role: 2
+  )
+end
+
+2.times do |u|
+  User.create!(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: "password",
+    role: 1
+  )
+end
