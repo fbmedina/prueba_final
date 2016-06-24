23bce9190fc17d162396df785e8f08b20dc7a7d8
Francisca Medina <fbmedina@uc.cl>
2016-06-23 20:10:33 -0400

    extension white list in photo uploader

--- app/uploaders/profile_photo_uploader.rb
+++ app/uploaders/profile_photo_uploader.rb
@@ -48,6 +48,10 @@ class ProfilePhotoUploader < CarrierWave::Uploader::Base
   #   %w(jpg jpeg gif png)
   # end
 
+  def extension_white_list
+    %w(jpg jpeg png)
+  end
+
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
   # def filename
