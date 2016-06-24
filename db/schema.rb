4dcd56df3426695e8017a831b559f3128aa71151
Francisca Medina <fbmedina@uc.cl>
2016-06-23 20:55:46 -0400

    Migrated new column that add ranking to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160623233943) do
+ActiveRecord::Schema.define(version: 20160624005428) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20160623233943) do
     t.string   "name"
     t.string   "profile_photo"
     t.integer  "role"
+    t.integer  "ranking"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
